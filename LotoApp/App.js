6f3e9992a78f446ae214b13343e6780e58d5ff15
Farhodbek1995 <farhodbek1995@gmail.com>
2026-06-29 14:36:42 +0500

    fix: App.js gesture handler import, native PNGs, versionCode 4

--- LotoApp/App.js
+++ LotoApp/App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
